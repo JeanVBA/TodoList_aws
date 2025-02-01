7e39a493c4a1a739ea9d10fe719fd53f89547a04
JeanVBA <jeanvictor999@outlook.com>
2025-02-01 15:10:06 -0300

    "config: Initialize project aws"

--- bin/todo_list_aws.ts
+++ bin/todo_list_aws.ts
@@ -3,6 +3,17 @@ import * as cdk from 'aws-cdk-lib';
 import { TodoListAwsStack } from '../lib/todo_list_aws-stack';
 
 const app = new cdk.App();
+
+const env: cdk.Environment = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+const tags = {
+  cost: "todo_list",
+  team: "devops",
+  project: "todo_list",
+};
+
 new TodoListAwsStack(app, 'TodoListAwsStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
