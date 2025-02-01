438e1478dfbd1aa14c9623cd86b684c892c38667
JeanVBA <jeanvictor999@outlook.com>
2025-02-01 14:41:23 -0300

    Initial commit

---
+++ test/todo_list_aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TodoListAws from '../lib/todo_list_aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/todo_list_aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TodoListAws.TodoListAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
